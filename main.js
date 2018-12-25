bd25c711874f552182435d530ea7292c64a075d8
KimmoKoodaa <kimmokoodaa@gmail.com>
2018-12-25 20:17:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -182,7 +182,7 @@ function haeUutiset(){
 					$('#uutisotsikotDiv').append("<tr><td>&nbsp;</td></tr>");
 				
 					// Tämä lisätään pääuutiseksi
-					if(otsikko === "Yhteenveto vuodesta 2018"){
+					if(otsikko === "Hyökkääjä-ikoni Villen viimeinen runkosarjapeli päättyi voittoon"){
 						$('#finaaliteksti').append(sisalto);
 					}		
 				});
