63f69d17025b3aecf9cec0b04b4dbe5aa738ef75
KimmoKoodaa <kimmokoodaa@gmail.com>
2019-01-08 13:13:22 +0200

    Update main.js

--- main.js
+++ main.js
@@ -182,7 +182,7 @@ function haeUutiset(){
 					$('#uutisotsikotDiv').append("<tr><td>&nbsp;</td></tr>");
 				
 					// Tämä lisätään pääuutiseksi
-					if(otsikko === "Hyökkääjä-ikoni Villen viimeinen runkosarjapeli päättyi voittoon"){
+					if(otsikko === "Vuosi on vaihtunut, katso tärpit vuodelle 2019"){
 						$('#finaaliteksti').append(sisalto);
 					}		
 				});
