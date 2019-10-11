ca7d7f482433da6bbe00028160a77f39be5eb1e3
KimmoKoodaa <kimmohietalahti@hotmail.com>
2019-10-11 15:24:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -182,7 +182,7 @@ function haeUutiset(){
 					$('#uutisotsikotDiv').append("<tr><td>&nbsp;</td></tr>");
 				
 					// Tämä lisätään pääuutiseksi
-					if(otsikko === "All Star -peli kaikkien aikojen trilleri!"){
+					if(otsikko === "Lokalatkinta toi kauteen vaihtelua"){
 						$('#finaaliteksti').append(sisalto);
 					}		
 				});
