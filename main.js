371790bae19d4d16590f692563c0d9790486fa86
KimmoKoodaa <kimmohietalahti@hotmail.com>
2020-01-26 18:12:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -182,7 +182,7 @@ function haeUutiset(){
 					$('#uutisotsikotDiv').append("<tr><td>&nbsp;</td></tr>");
 				
 					// Tämä lisätään pääuutiseksi
-					if(otsikko === "Kokemus näkyi 20-luvun ensimmäisissä otteluissa"){
+					if(otsikko === "Tähtiketju jälleen voittoon - Nyt alivoimalla!"){
 						$('#finaaliteksti').append(sisalto);
 					}		
 				});
